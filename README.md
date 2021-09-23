008f8bd0516ba0bbb08cbdddc45290b96db4e656
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2021-09-23 12:29:41 -0500

    feat: initialize project
    Added configuration files and project structure.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flex (Lex) Compiladores Lab MonoRepo
+
+Repositorio para laboratorios de Compiladores.
+
+## Autores
+
+- Jesus Imitola
+- Jesus Reyes
+- Luis Camacho
---
+++ scripts/build-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PROJECT=$1
+COMP_PATH="./Lab$1"
+
+lex -o ${COMP_PATH}/dist/lex.yy.c ${COMP_PATH}/lab$1.l
+gcc ${COMP_PATH}/dist/lex.yy.c -o ${COMP_PATH}/dist/a.out
+${COMP_PATH}/dist/a.out

005707ea855ad941f76c1cfcae053cbbe2c63fd4
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2021-10-08 16:34:23 -0500

    fix: change binary file name

--- scripts/build-run.sh
+++ scripts/build-run.sh
@@ -4,4 +4,4 @@ PROJECT=$1
 COMP_PATH="./Lab$1"
 
 lex -o ${COMP_PATH}/dist/lex.yy.c ${COMP_PATH}/lab$1.l
-gcc ${COMP_PATH}/dist/lex.yy.c -o ${COMP_PATH}/dist/a.out
+gcc ${COMP_PATH}/dist/lex.yy.c -o ${COMP_PATH}/dist/binario.out

ae0a2b6c53b52317ef42addaa67044e7a1d8fbc2
Jesus Reyes <reyesjd@uninorte.edu.co>
2021-11-09 13:34:46 -0500

    Cicles and if added

--- scripts/build-lab3-run.sh
+++ scripts/build-lab3-run.sh
@@ -2,6 +2,6 @@
 
 COMP_PATH="./Lab3"
 
-yacc -d ${COMP_PATH}/lab3.y -o ${COMP_PATH}/dist/y.tab.c
+yacc -d -v ${COMP_PATH}/lab3.y -o ${COMP_PATH}/dist/y.tab.c
 lex -o ${COMP_PATH}/dist/lex.yy.c ${COMP_PATH}/lab3.l 
 cc ${COMP_PATH}/dist/y.tab.c ${COMP_PATH}/dist/lex.yy.c -o ${COMP_PATH}/dist/a.out
